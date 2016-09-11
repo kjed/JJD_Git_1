4fb30a6cf6fe5cc216274505741591cf687b7d1d
Krzysztof Jedrzejewski <krzysztof.jedrzejewski.gd@gmail.com>
2016-09-11 16:30:14 +0200

    add new patient

--- src/main/java/pl/michalczukm/isa/java/PatientService.java
+++ src/main/java/pl/michalczukm/isa/java/PatientService.java
@@ -11,7 +11,7 @@ public class PatientService {
 
         patients.add(new Patient("Jan", "Kowalski"));
         patients.add(new Patient("Adam", "Nowak"));
-        patients.add(new Patient("Piotr", "Szulc"));
+        patients.add(new Patient("Kamil", "Adamowski"));
     }
 
     public List<Patient> getAll() {
